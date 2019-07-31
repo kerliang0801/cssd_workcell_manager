db9a74d1fd3f175707f03658b55e2ca0a55211fb
malcom-rmf <malcom.neo@hopetechnik.com>
2019-07-31 16:08:14 +0800

    updated header

--- cssd_workcell_manager/include/cssd_workcell_manager.h
+++ cssd_workcell_manager/include/cssd_workcell_manager.h
@@ -66,10 +66,13 @@ private:
   rclcpp::Subscription<rmf_msgs::msg::DispenserRequest>::SharedPtr DispenserRequest_;
   rclcpp::Subscription<rmf_msgs::msg::DispenserResult>::SharedPtr RAWMRespond_;  
   rclcpp::Subscription<rmf_msgs::msg::DispenserState>::SharedPtr RAWMState_;    
+  
   rclcpp::Publisher<rmf_msgs::msg::InventoryCheckResponse>::SharedPtr InventoryCheckResponse_;
   rclcpp::Publisher<rmf_msgs::msg::DispenserRequest>::SharedPtr RAWMRequest_;
   rclcpp::Publisher<rmf_msgs::msg::DispenserResult>::SharedPtr DispenserResponse_;
+  
   rclcpp::Client<xbee_interface::srv::R2R>::SharedPtr R2R_client_;
+  
   rclcpp::callback_group::CallbackGroup::SharedPtr RAWM_callback_group_;
   rclcpp::callback_group::CallbackGroup::SharedPtr RFM_callback_group_;
   rclcpp::callback_group::CallbackGroup::SharedPtr R2R_group_;
@@ -95,33 +98,40 @@ private:
 	std::vector<sub_workcell> RAWM;
 	bool new_request = false;
 	std::string request_id;
-	std::string transporter_id; //note down the transporter id that is requested
+	std::string transporter_id; 
 	std::vector<bool> trolley_compartment_status;
 	std::vector<std::string> trolley_compartment_id;
 
   void inventory_check_callback(const rmf_msgs::msg::InventoryCheckRequest::SharedPtr msg);
-  //check inventory when OTUI request for a set of item. Will sort through the requested item and add to the queue of the relevant RAWM
+  /*check DB inventory when OTUI request for a set of item. Will make sure that the requested item is below the limit set in yaml.
+  Will sort through the requested item and add to the queue of the relevant RAWM
+  */
+	
+	void dispenser_request_callback(const rmf_msgs::msg::DispenserRequest::SharedPtr msg);
+  /*callback for RFM dispenser request. Will query R2R service, checking that the trolley is closeby and save the status and id of compartment.
+  Will check that the trolley has enough space. Save the request_id to the cssd_wm class and set "new_request" to true, 
+  allowing cssd:main() to start publishing to RAWM, according to the request id.
+  */
 
   void RAWM_respond_callback(const rmf_msgs::msg::DispenserResult::SharedPtr msg);
-  //update RAWM status when RAWM send response
-
+  /*update RAWM status when RAWM send response. It contain an R2R query to make sure that the compartment the arm placed has an item on it. 
+	This is to check that the item is not dropped along the way. If not successfully loaded, the status will turn 2 and the cssd:main will handle it.
+	*/
   void RAWM_state_callback(const rmf_msgs::msg::DispenserState::SharedPtr msg);
-  //update RAWM status when RAWM send periodic status updates
+  // periodically update RAWM status when RAWM send periodic status updates.
 
-  void dispenser_request_callback(const rmf_msgs::msg::DispenserRequest::SharedPtr msg);
-  //callback for RFM dispenser request. save the request_id to the cssd_wm class and set "new_request" to true, allowing cssd:main() to start publishing to RAWM
-
+  
   void failed_loading_handling(std::string request_id);
-  //handle RAWM failure. called in cssd_wm::main() when the status of any RAWM is 2.
-  //Will release all leftover item in the request_id
-
+  /*handle RAWM failure. called in when error occur such as when the status of any RAWM is 2.
+  Will release all leftover item in the request_id.
+	*/
   bool R2R_query(std::string device_id, 
                 std::vector<bool>& compartment_status,
                 std::vector<std::string>& compartment_id);
   //query to get R2R info
   
   void send_result_to_meta_FMS(bool result);
-
+  //send the result accordingly to the result arg.
 
 };
 
